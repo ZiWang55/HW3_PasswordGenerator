d669b3c342659e3f27c10652734bfd9ee7b3b406
zi wang <71153277+ZiWang55@users.noreply.github.com>
2020-10-12 17:55:14 -0500

    added working html and css file, created javascript file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HW3_PasswordGenerator
+
+## Overview
+
+* Using HTML CSS Boostrap and Javascript, creat a simple website that functions as the user wants with selecting with length and type of passwords, while offering a easy copy to clipboard function.
---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// prompt length. use alert data for function
+const passwordLength = document.getElementById("passwordSetting");
+// let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+// generate password function, choose length and settings
+
+fucntion passwordSetting(){
+    var userAdjective = prompt("choose password length");
+    alert (userAdjective);
+}
+
+// function generate(){
+
+//     let complexity = document.getElementById("").Value;
+
+//     let values = "";
+
+//     // create for loop to choose password
+//     for(var i = 0; i <= complexity; i++){
+//         password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+//     }
+// }
+
+// Password requirement prompt
+// length 4-100
+// symbols
+// upper or lower case
+// numbers
+
+// press generate pass word
+
+// make copy button work
+
